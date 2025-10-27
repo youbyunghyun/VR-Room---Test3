88b351fde8c66a53acf2f23502032f50d81da6ba
youbyunghyun <dbdbdb4444@gmail.com>
2025-10-27 15:02:33 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,251 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.18722145164399429,
+        "pitch": 0.10654855217888759,
+        "fov": 1.3918202015316714
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.457785582444732,
+          "pitch": 0.2449648147459733,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -1.3633261387424955,
+          "pitch": 0.3492203468884387,
+          "rotation": 8.63937979737193,
+          "target": "2-3"
+        },
+        {
+          "yaw": 2.855759578291516,
+          "pitch": 0.35222250349298534,
+          "rotation": 3.141592653589793,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.45307551573615257,
+          "pitch": -0.10270957893600396,
+          "title": "Hood",
+          "text": "Frank"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.522855723562122,
+          "pitch": 0.2696306996125504,
+          "rotation": 1.5707963267948966,
+          "target": "2-3"
+        },
+        {
+          "yaw": 2.388213003765518,
+          "pitch": 0.3703529521617952,
+          "rotation": 4.71238898038469,
+          "target": "0-1"
+        },
+        {
+          "yaw": -0.1438618408119794,
+          "pitch": 0.27057943909070303,
+          "rotation": 1.5707963267948966,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.033007410594818,
+          "pitch": 0.1284612736488473,
+          "title": "Kitchen-1",
+          "text": "Black Locker"
+        },
+        {
+          "yaw": 0.13849037349195115,
+          "pitch": 0.29077056636520027,
+          "title": "Kitchen-2",
+          "text": "White Glass"
+        },
+        {
+          "yaw": 0.8250816070034901,
+          "pitch": 0.10560837343319207,
+          "title": "Kitchen-3",
+          "text": "Gold Locker"
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.121749002351704,
+          "pitch": 0.2156495926716211,
+          "rotation": 6.283185307179586,
+          "target": "5-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.750958436562625,
+          "pitch": 0.2382275057504124,
+          "rotation": 4.71238898038469,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
